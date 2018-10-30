9b017f34c453bafe751b31ae4ffef305bc28cf7c
Damodar Dahal <damodar.dahal@protonmail.com>
2018-10-29 23:14:25 -0500

    create not-found route in search

--- src/app/routes/NotFoundRoute/NotFoundRoute.js
+++ src/app/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,8 +1,24 @@
 import DocumentMeta from 'react-helmet'
 import { hot } from 'react-hot-loader'
 import { notFoundRoute } from 'app/copy'
+import { Redirect } from 'react-router-dom'
+import { SERVER_ROOT } from '../../../helpers/url-helper'
 
 class NotFoundRoute extends React.Component {
+  // TODO remove this
+  componentDidMount() {
+    const { pathname } = this.props.location
+    const w = window
+
+    const a = w.document.createElement('a')
+    a.href = `${SERVER_ROOT}${pathname}`
+
+    if (!a.pathname.endsWith('/')) {
+      a.pathname += '.php'
+    }
+    window.location = a.href
+  }
+
   render() {
     return (
       <section className='NotFoundRoute'>
