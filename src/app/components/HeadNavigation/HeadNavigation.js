d143218635cf1345030daba3658c35f3b016d414
Damodar Dahal <damodar.dahal@protonmail.com>
2019-01-27 10:14:39 -0600

    hide menu on menu item click on phones

--- src/app/components/HeadNavigation/HeadNavigation.js
+++ src/app/components/HeadNavigation/HeadNavigation.js
@@ -4,18 +4,29 @@ import { nav as navCopy } from 'app/copy'
 import styles from './HeadNavigation.module.scss'
 import QuickSearchBox from '../QuickSearchBox/QuickSearchBox'
 import headerIcon from 'assets/ErmIcon.png'
+import {hot} from 'react-hot-loader'
+
 
 // Putting this inside a connect will break activeClassName
 // unless you also subscribe to changes to routing state or context
-export default class HeadNavigation extends React.Component {
+export class HeadNavigation extends React.Component {
+
   constructor(props) {
     super(props)
 
 
     this.toggleIndices = this.toggleIndices.bind(this)
+    this.handleMenuClick = this.handleMenuClick.bind(this)
 
     this.state = {
       indicesExpanded: false,
+    }
+  }
+
+  handleMenuClick(e){
+    const toggler = $('.navbar-toggler')
+    if (toggler.is(':visible')) {
+      toggler.click();
     }
   }
 
@@ -38,7 +49,11 @@ export default class HeadNavigation extends React.Component {
         </div>
 
         <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
-          <NavLink to='/' className='navbar-brand'>
+          <NavLink
+            to='/'
+            className='navbar-brand'
+            onClick={this.handleMenuClick}
+          >
             <img src={headerIcon} alt='Early Ruskin Manuscripts' />
           </NavLink>
 
@@ -53,13 +68,16 @@ export default class HeadNavigation extends React.Component {
             <span className='navbar-toggler-icon' />
           </button>
 
-          <div className='collapse navbar-collapse' id='navbarsExampleDefault'>
+          <div
+            className='collapse navbar-collapse'
+            id='navbarsExampleDefault'>
             <ul className='navbar-nav mr-auto'>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/'
-                  className='nav-link'>
+                  className='nav-link'
+                  onClick={this.handleMenuClick}>
                   {navCopy.home}
                 </NavLink>
               </li>
@@ -70,6 +88,7 @@ export default class HeadNavigation extends React.Component {
                   aria-expanded='false'
                   aria-haspopup='true'
                   id='dropdown01'
+                  onClick={this.handleMenuClick}
                   data-toggle='dropdown'>
                   {navCopy.indices}
                 </NavLink>
@@ -77,49 +96,77 @@ export default class HeadNavigation extends React.Component {
                   className='dropdown-menu'
                   aria-labelledby='dropdown01'
                   aria-expanded='false'>
-                  <NavLink to='/essays/indices_essay#WORKS' className='dropdown-item'>{navCopy.works}</NavLink>
-                  <NavLink to='/essays/indices_essay#MANUSCRIPTS' className='dropdown-item'>{navCopy.manuscripts}</NavLink>
-                  <NavLink to='/essays/indices_essay#CORPORA' className='dropdown-item'>{navCopy.corpora}</NavLink>
-                  <NavLink to='/essays/indices_essay#WORKSBYOTHERS' className='dropdown-item'>{navCopy.workByOthers}</NavLink>
-                  <NavLink to='/essays/indices_essay#COMMENTARY' className='dropdown-item'>{navCopy.commentary}</NavLink>
-                  <NavLink to='/essays/indices_essay#ESSAYS' className='dropdown-item'>{navCopy.essays}</NavLink>
-                  <NavLink to='/essays/indices_essay#DRAWINGS' className='dropdown-item'>{navCopy.drawings}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#WORKS'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.works}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#MANUSCRIPTS'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.manuscripts}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#CORPORA'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.corpora}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#WORKSBYOTHERS'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.workByOthers}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#COMMENTARY'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.commentary}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#ESSAYS'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}>{navCopy.essays}</NavLink>
+                  <NavLink
+                    to='/essays/indices_essay#DRAWINGS'
+                    className='dropdown-item'
+                    onClick={this.handleMenuClick}
+                  >{navCopy.drawings}</NavLink>
                 </div>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/xml'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>{navCopy.xml}</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/notes/bibliography'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>{navCopy.bibliography}</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/webpages/staff'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>{navCopy.staffAndSupport}</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/webpages/legal'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>{navCopy.legal}</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/search'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>{navCopy.search}</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink
                   exact
                   to='/update_db'
+                  onClick={this.handleMenuClick}
                   className='nav-link'>Update db</NavLink>
               </li>
             </ul>
@@ -136,3 +183,5 @@ export default class HeadNavigation extends React.Component {
     )
   }
 }
+
+export default hot(module)(HeadNavigation)
