9de636b24a92629b1b7f469d250546ce2e15dfcf
Damodar Dahal <damodar.dahal@protonmail.com>
2019-01-27 23:39:03 -0600

    ui updates

--- src/helpers/url-helper.js
+++ src/helpers/url-helper.js
@@ -1,5 +1,6 @@
 // export const SERVER_ROOT = 'http://localhost:9001'
 export const SERVER_ROOT = 'http://ruskin.local:8080/src'
+// export const SERVER_ROOT = 'http://192.168.0.163:8080/src'
 
 export function getUrl(path) {
   return `/${path}`
