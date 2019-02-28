10838ebfd4e6fa3f78e3af4ebabe9ca6255466bb
Damodar Dahal <damodar.dahal@protonmail.com>
2019-02-27 22:36:47 -0600

    update showcase directory

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ export const CONTAINER_ELEMENT_ID = 'app-container'
 
 export const SERVER_ROOT = 'http://ruskin.english.selu.edu:8080'
 export const SERVER_XML_DIRECTORY = `${SERVER_ROOT}/web/xml`
-export const SERVER_SHOWCASE_DIRECTORY = `${SERVER_ROOT}/web/showcase`
+export const SERVER_SHOWCASE_DIRECTORY = `${SERVER_ROOT}/web/pages`
 export const SERVER_HTML_DIRECTORY = `${SERVER_ROOT}/web/pages`
 
 export const GA_TRACKING_ID = 'UA-134616383-1'
