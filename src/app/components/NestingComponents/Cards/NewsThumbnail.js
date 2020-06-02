8fe8ce0e68af3ffe2fe73f10febcdb061794882a
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-06-02 14:54:47 -0500

    Deletion Completed on Static FrontEnd
    Next step: hit /deleteNews/ID

--- src/app/components/NestingComponents/Cards/NewsThumbnail.js
+++ src/app/components/NestingComponents/Cards/NewsThumbnail.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button,Card } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -8,101 +8,67 @@ import Modal from 'react-bootstrap/Modal'
 let axios = require('axios')
 
 
-export class Thumbnail extends React.Component{
+export class Thumbnail extends React.Component {
 
   constructor(props) {
-    super(props);
-    this.state={
-      _id:props.props._id,
-      title:props.props.title,
-      imgUrl:props.props.imgUrl,
-      description:props.props.description,
-      deletionItemId:null,
-      show:false,
-    }
+    super(props)
   }
+
+
   render() {
-    const handleClose = () => this.setState({show:false});
-    const handleShow = () => {
-      this.setState({show:true});
+    const decision=()=>{
+      if (confirm('Are you sure you want to delete?'+ this.props.props.title)) {
+      // Save it!
+        this.props.deleteItem(this.props.props._id)
+      } else {
+      // Do nothing!
+      }
 
     }
-    const decision =()=>{
-      // axios.delete(`/deleteNews/${this.state._id}`)
-      //   .then(res => {
-      //     console.log(res)
-      //     this.setState({title:null})
-      //     this.setState({imgUrl:null})
-      //     this.setState({description:null})
-      //     handleClose();
-      //   })
-      this.props.deleteItem(this.state._id)
-      handleClose();
-
-    }
-
-
     return (
-      <Card>
-        <Card.Img variant="top" src={`${this.state.imgUrl}`} />
-        <Card.Body>
-          <Card.Title>{this.state.title}</Card.Title>
-          <Card.Text>
-            {this.state.description.replace(/^(.{200}[^\s]*).*/, "$1") }
-          </Card.Text>
+      <div>
+        <Card>
+          <Card.Img variant='top' src={`${this.props.props.imgUrl}`} />
+          <Card.Body>
+            <Card.Title>{this.props.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.props.description.replace(/^(.{200}[^\s]*).*/, '$1') }
+            </Card.Text>
 
-          <Row>
-            <Col>
-              <Button variant="primary" >
-                <Link to={{ pathname: '/news/newsOne',
-                  state:  this.state }}>
-                  Read More
-                </Link>
-              </Button>
-            </Col>
-            <Col>
-              {/*this code executes only if the admin is in /edithNews route*/}
-              {document.location.pathname.indexOf('editNews')>0?(
-                <Button variant="warning" >
-                  <Link to={{ pathname: `/editPost/${this.state._id}`,
-                    state:  this.state }}>
-                    Edit Post
-                  </Link>
-                </Button>
-              ):('')}
-            </Col>
-            <Col>
-              {/*this code executes only if the admin is in /edithNews route*/}
-              {document.location.pathname.indexOf('editNews')>0?(
-                <Button variant="danger"  onClick={handleShow}>
-                   Delete
-                </Button>
-              ):('')}
-            </Col>
-          </Row>
-
-        </Card.Body>
-        <Modal show={this.state.show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Do you want to delete this post?</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{this.state.title}</Modal.Body>
-          <Modal.Footer>
             <Row>
               <Col>
-                <Button variant="secondary" onClick={decision}>
-                  Yes
+                <Button variant='primary' >
+                  <Link to={{ pathname: '/news/newsOne',
+                    state:  this.props.props }}>
+                  Read More
+                  </Link>
                 </Button>
               </Col>
               <Col>
-                <Button variant="primary" onClick={handleClose}>
-                  No
-                </Button>
+                {/*this code executes only if the admin is in /edithNews route*/}
+                {document.location.pathname.indexOf('editNews')>0?(
+                  <Button variant='warning' >
+                    <Link to={{ pathname: `/editPost/${this.props.props._id}`,
+                      state:  this.props.props }}>
+                    Edit Post
+                    </Link>
+                  </Button>
+                ):('')}
+              </Col>
+              <Col>
+                {/*this code executes only if the admin is in /edithNews route*/}
+                {document.location.pathname.indexOf('editNews')>0?(
+                  <Button variant='danger' onClick={()=>decision()}>
+                  Delete
+                  </Button>
+                ):('')}
               </Col>
             </Row>
-          </Modal.Footer>
-        </Modal>
-      </Card>
-    );
+          </Card.Body>
+        </Card>
+      </div>
+    )
+
   }
+
 }
--- src/app/components/News/News.js
+++ src/app/components/News/News.js
@@ -1,71 +1,50 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import {Thumbnail} from '../NestingComponents/Cards/NewsThumbnail'
+import { Thumbnail } from '../NestingComponents/Cards/NewsThumbnail'
 
 export class News extends React.Component {
   constructor(props) {
-    super(props);
-    this.state={
-      news:[]
+    super(props)
+    this.state = {
+      news: [],
     }
+
   }
 
 
-
   componentDidMount() {
-    fetch('/getNews').then((response=>response.json())).then(result=>{
+    fetch('/getNews').then((response => response.json())).then(result => {
       console.log('what?')
-      this.setState({news:result});
-      console.log(this.state.news)
+      this.setState({ news: result })
     })
 
   }
 
+
   render() {
-    const deleteItem =(id) =>{
-      // let copyState =this.state.news.slice();
-      // copyState.map((item,index)=>{
-      //   if (item._id===id){
-      //     copyState.splice(index ,1);
-      //
-      //     copyState.map((data)=>{
-      //       console.log('---------')
-      //       console.log(data.title)
-      //     })
-      //
-      //     this.setState({news:[]})
-      //     this.state.news.map((data)=>{
-      //       console.log('))))))))))')
-      //       console.log(data.title)
-      //     })
-      //
-      //   }
-      // })
-
-
-
-      this.state.news.map((item,index)=>{
-        if (item._id===id){
-          this.setState({news:this.state.news.splice(index ,1)})
-
-          console.log(this.state.news.length)
-        }
+    const deleteSingleNews=(id) =>{
+      let newsData = this.state.news.filter((item)=>{
+        if (item._id!==id) return item
       })
-
-
-
+      this.setState({news:newsData})
     }
     return (
       <Container>
         <Row>
-          {this.state.news?
-            this.state.news.map((item,index)=>(
+          {this.state.news
+            ? this.state.news.map((item, index) => (
               <Col xs={12} md={6}>
-                <Thumbnail props={item} deleteItem ={deleteItem} keys={index}/>
+                {/*<button onClick={()=>deleteSingleNews(item._id)}>{item.title}</button>*/}
+                <Thumbnail
+                  props={item}
+                  key={item._id}
+                  deleteItem={deleteSingleNews}
+                />
+
+
               </Col>
             ))
-            :
-            (<span>no data</span>)}
+            : (<span>no data</span>)}
         </Row>
       </Container>
     )
